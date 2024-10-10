646ca7990d17d4b4e267a56b49ba3d41897db939
Vedant Yuvaraj Desale <2023ucp1584@mnit.ac.in>
2024-10-10 15:09:40 +0530

    Om

---
+++ ps.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Define two vectors
+vector_a = np.array([1, 2, 3])
+vector_b = np.array([4, 5, 6])
+
+# Print the vectors
+print("Vector A:", vector_a)
+print("Vector B:", vector_b)
+
+# Calculate the dot product
+dot_product = np.dot(vector_a, vector_b)
+
+# Print the dot product
+print("Dot Product:", dot_product)
+
